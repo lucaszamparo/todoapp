e0228e3880722ced9078d15500cc2ea319edb153
lucaszamparo <60469044+lucaszamparo@users.noreply.github.com>
2020-04-12 18:28:23 -0300

    Add files via upload

---
+++ tempData.js
@@ -0,0 +1,70 @@
+// Temporary Data until we connect with firebase
+
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: '#24A6D9',
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: '#8022D9',
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Plan weekend camping trip",
+                completed: true
+            },
+            {
+                title: "Run 2 miles",
+                completed: true
+            },
+            {
+                title: "Make a video",
+                completed: true
+            },
+            {
+                title: "Take dog for a walk",
+                completed: true
+            }
+        ]
+    },
+    {
+        name: "Birthday party",
+        color: '#595BD9',
+        todos: [
+            {
+                title: "Get Baloons",
+                completed: false
+            },
+            {
+                title: "Send Invitations",
+                completed: true
+            },
+            {
+                title: "Make Dinner Reservations",
+                completed: true
+            }
+        ]
+    }
+]
